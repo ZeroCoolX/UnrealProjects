37e24eed9feea9fde7229b350104fdd021d77ee4
PattonCooper <TheHerbaliser@192.168.1.29>
2016-08-26 17:46:18 -0500

    PROJECT_02 - Building Escape - fixed the crashing problem it was user error...Also added in spotlight and triggervolume to begin solving the puzzle of how to get out of the room

--- BuildingEscape/Source/BuildingEscape/OperateDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OperateDoor.cpp
@@ -7,46 +7,57 @@
 // Sets default values for this component's properties
 UOperateDoor::UOperateDoor()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	bWantsBeginPlay = true;
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    bWantsBeginPlay = true;
+    PrimaryComponentTick.bCanEverTick = true;
+    
+    // ...
 }
 
 
 // Called when the game starts
 void UOperateDoor::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
+    
+    //get owner which many methods need
+    owner = GetOwner();
+    
+    //find the player controller
+    actorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();//pawn IS A actor
 }
 
 void UOperateDoor::OpenDoor(){
-    //get owner
-    AActor* owner = GetOwner();
-    
-    //create a rotator
-    FRotator newRotation = FRotator(0.0f, 60.0f, 0.0f);
-    
     //set door rotation
-    UE_LOG(LogTemp, Warning, TEXT("Setting rotation"));
-    owner->SetActorRotation(newRotation);
+    UE_LOG(LogTemp, Warning, TEXT("Opening door"));
+    owner->SetActorRotation(FRotator(0.0f, openAngle, 0.0f));
+}
+
+void UOperateDoor::CloseDoor(){
+    //set door rotation
+    UE_LOG(LogTemp, Warning, TEXT("Closing door"));
+    owner->SetActorRotation(FRotator(0.0f, 0.0f, 0.0f));
 }
 
 
 // Called every frame
 void UOperateDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
-	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
-
-	//Poll the trigger every frame
-    if(pressurePlate->IsOverlappingActor(actorThatOpens)){
+    Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
     
-    }
-   /* if(pressurePlate->IsOverlappingActor(actorThatOpens)){
+    //Poll the trigger every frame
+    if(pressurePlate->IsOverlappingActor(actorThatOpens)){
         UE_LOG(LogTemp, Warning, TEXT("Opening door because the trigger was activated!!"));
         OpenDoor();
-    }*/
+        lastDoorOpenTime = GetWorld()->GetTimeSeconds();
+    }
+    
+    //check if its time to close the door
+    if(lastDoorOpenTime+doorCloseDelay < GetWorld()->GetTimeSeconds()){
+        UE_LOG(LogTemp, Warning, TEXT("Closing door because it was open too long"));
+        CloseDoor();
+    }
+    
 }
 
--- BuildingEscape/Source/BuildingEscape/OperateDoor.h
+++ BuildingEscape/Source/BuildingEscape/OperateDoor.h
@@ -9,20 +9,23 @@
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOperateDoor : public UActorComponent
 {
-	GENERATED_BODY()
-
-public:	
-	// Sets default values for this component's properties
-	UOperateDoor();
-
-	// Called when the game starts
-	virtual void BeginPlay() override;
-	
-	// Called every frame
-	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+    GENERATED_BODY()
+    
+public:
+    // Sets default values for this component's properties
+    UOperateDoor();
+    
+    // Called when the game starts
+    virtual void BeginPlay() override;
+    
+    // Called every frame
+    virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
     
     //opens a door 90 degrees
     void OpenDoor();
+    
+    //close a door 90 degrees
+    void CloseDoor();
     
     
 private:
@@ -32,10 +35,13 @@ private:
     UPROPERTY(EditAnywhere)
     ATriggerVolume* pressurePlate;
     
-    UPROPERTY(EditAnywhere)
     AActor* actorThatOpens;//remember pawn inherits from actor
+    AActor* owner;
     
-
-		
-	
+    UPROPERTY(EditAnywhere)
+    float doorCloseDelay = 1.0f;
+    
+    float lastDoorOpenTime;
+    
+    
 };
